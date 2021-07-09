28d40b3862b44d45fb43870682759dc0539a2f15
TheAirBlow <pilexet.danila@gmail.com>
2021-07-09 16:17:12 +0400

    Ловите ядро нахуй (v.0.3)

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -1,13 +1,12 @@
 #include "utils/kernelUtil.h"
 
 extern "C" void _start(BootInfo* bootInfo){
-
     KernelInfo kernelInfo = InitializeKernel(bootInfo);
     PageTableManager* pageTableManager = kernelInfo.pageTableManager;
 
     GlobalVga->Next();
     GlobalVga->Colour = 0xFFFF00;
-    GlobalVga->Print("AirKernel v0.2 loaded!");
+    GlobalVga->Print("AirKernel v0.3 loaded!");
     GlobalVga->Colour = 0xFFFFFF;
     GlobalVga->Next();
 
